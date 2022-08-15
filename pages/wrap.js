cdffda98d87cefab12424871794dd3f93233e872
zoubair.hattab <elhattabzoubair93@gmail.com>
2022-08-15 16:52:11 +0100

    task

--- pages/wrap.js
+++ pages/wrap.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import abi from "../public/artifacts/contracts/CrowdFunding.sol/CrowdFunding.json";
 import abireward from "../public/artifacts/contracts/reword/abi.json"
+import abimanager from "../public/artifacts/contracts/Manager.sol/Manager.json"
+
 
 export default function Wrap() {
   const [amount, setAmount] = useState("");
@@ -12,6 +14,7 @@ export default function Wrap() {
   const web3 = new Web3(window.ethereum);
    const  contract=   new web3.eth.Contract(abi.abi,"0x9EB1c5ADC803D9Ef9E921062B0866aF90ce00B8C");
    const  contract1=   new web3.eth.Contract(abireward,"0xc5738334b972745067fFa666040fdeADc66Cb925");
+   const  contract2=   new web3.eth.Contract(abimanager.abi,"0x13eFbD633570678603d994959f2131d2153A508C");
 
    console.log(contract.methods)
   useEffect(() => {
@@ -27,7 +30,7 @@ export default function Wrap() {
   }, [amount]);
 
   const wrap = async () => {
-if (contract){   
+
   const _epochIds = await contract1.methods
   .getEpochsWithUnclaimedRewards(account[0])
   .call();
@@ -36,7 +39,7 @@ if (contract){
       from:account[0],
       gas:300000
     })
-    }
+    
 
   };
   return (
