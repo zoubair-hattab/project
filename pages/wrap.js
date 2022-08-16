04f73ffda6df31e3efe490ca5cd25bed2589606e
zoubair.hattab <elhattabzoubair93@gmail.com>
2022-08-16 08:00:40 +0100

    kk

--- pages/wrap.js
+++ pages/wrap.js
@@ -25,10 +25,10 @@ export default function Wrap() {
       setAccount(_account);
       const _balance = await web3.eth.getBalance(_account[0]);
       setBalance(_balance);
-      const {..._epochIds} = await contract.methods
-     .epochsWithUnclaimedStakerReward (_account[0])
-  .call();
-    console.log({..._epochIds});
+      const _epochIds = await contract1.methods
+      .getEpochsWithUnclaimedRewards (account[0])
+      .call();
+    setEpochIds(_epochIds);
     }
     init();
   }, [amount]);
@@ -36,8 +36,8 @@ export default function Wrap() {
   const wrap = async () => {
 
   
-  //  console.log(epochIds)
-    await contract.methods.distributeFtsoRewardsToProject(epochIds).send({
+    console.log(epochIds)
+    await contract.methods.withdrawInitialCapital().send({
       from:account[0],
       gas:300000
     })
