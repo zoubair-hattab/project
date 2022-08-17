6a42c23e50a325701f8152e42deb5d40458a71bd
zoubair.hattab <elhattabzoubair93@gmail.com>
2022-08-17 14:27:30 +0100

    kk

--- pages/wrap.js
+++ pages/wrap.js
@@ -12,7 +12,7 @@ export default function Wrap() {
   const [epochIds, setEpochIds] = useState([]);
   const [provider,setProvider]=useState([]);
   const web3 = new Web3(window.ethereum);
-   const  contract=   new web3.eth.Contract(abi.abi,"0x66d6B810904DEa0BA431Aa7Be4B720FEc4d3b01A");
+   const  contract=   new web3.eth.Contract(abi.abi,"0x61A4bc6c95691F9C38542F0790842bb694dD7c23");
    //const contract1 = new web3.eth.Contract(abis.abi, "0x786Ef989df463483770343717a4F6ae45f18de97");
 
    console.log(contract.methods)
