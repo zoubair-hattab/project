18b7933d053ef44931e2fdb4c6ceb12418e88b73
zoubair.hattab <elhattabzoubair93@gmail.com>
2022-08-18 11:06:59 +0100

    mm

--- pages/wrap.js
+++ pages/wrap.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import abi from "../public/artifacts/contracts/CrowdFunding.sol/CrowdFunding.json";
-import manager from "../public/artifacts/contracts/Manager.sol/Manager.json";
-import abis from "../public/artifacts/contracts/wnat/abi.json";
-import distrib from "../public/artifacts/contracts/distrubtion/distribution.json";
+
 
 export default function Wrap() {
+
   const [amount, setAmount] = useState("");
   const [balance, setBalance] = useState(0);
   const [account, setAccount] = useState("");
@@ -14,13 +13,10 @@ export default function Wrap() {
   const [fee,setFee]=useState([]);
   const [bips,setBips]=useState([]);
 
-  const web3 = new Web3(window.ethereum);
-  // const  contract=   new web3.eth.Contract(abi.abi,"0x66d6B810904DEa0BA431Aa7Be4B720FEc4d3b01A");
+  // const  contract2=   new web3.eth.Contract(manager.abi,"0xc3A6Cc6Aef27D7F9BF3706833c6bdB6e37406CD8");
    //const contract1 = new web3.eth.Contract(abis, "0xc5738334b972745067fFa666040fdeADc66Cb925");
-   //const  contract2=   new web3.eth.Contract(manager.abi,"0xc3A6Cc6Aef27D7F9BF3706833c6bdB6e37406CD8");
-   const  contract3=   new web3.eth.Contract(distrib,"0x32c6379B2978A9aB75993cA82e3ADc77dd50010C");
-console.log(contract3.methods)
-  useEffect(() => {
+
+/*   useEffect(() => {
     async function init() {
       const chainId = await web3.eth.getChainId();
      
@@ -29,36 +25,31 @@ console.log(contract3.methods)
       const _balance = await web3.eth.getBalance(_account[0]);
       setBalance(_balance);
 
-    /*   const _epochIds = await contract1.methods
+      const _epochIds = await contract1.methods
       .getEpochsWithUnclaimedRewards("0x66d6B810904DEa0BA431Aa7Be4B720FEc4d3b01A")
       .call();
       setEpochIds("epochIds",_epochIds);
     const valus=  await contract2.methods.getDistributions().call()
-      console.log("getDistributions",valus) */
-      const dis=  await contract3.methods.getCurrentMonth().call()
-      console.log("getCurrentMonth",dis)
-      const disp=  await contract3.methods.getMonthToExpireNext().call()
-      console.log("getMonthToExpireNext",disp)
+      console.log("getDistributions",valus)
      
     }
     init();
-  }, [amount]);
-console.log(epochIds)
+  }, [amount]); */
 
   const change = async () => {
-   /*   await contract1.methods.changeDelegations([provider],[10000])
+ /*     await contract1.methods.changeDelegations([provider],[10000])
     .send({
       from: account[0],
      // value: amount * 10 ** 18,
       gas:3000000,
-    })  */
- 
+    }) 
+  */
     
   };
   
 
   const claim = () => {
-/*     contract.methods
+  /*   contract.methods
       .distributeFtsoRewardsToProject(epochIds)
       .send({
         from: account[0],
@@ -67,13 +58,22 @@ console.log(epochIds)
   };
 
   const wrap = async () => {
-  /*   await contract.methods
+
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+      const web3 = window.web3;
+      var _account = await web3.eth.getAccounts();
+      setAccount(_account[0]);
+      const _balance = await web3.eth.getBalance(_account[0]);
+      setBalance(_balance);
+      const  contract=   new web3.eth.Contract(abi.abi,"0x66d6B810904DEa0BA431Aa7Be4B720FEc4d3b01A");
+     await contract.methods
     .fund()
     .send({
       from: account[0],
       value: amount * 10 ** 18,
       gas:3000000,
-    })  */
+    }) 
    
 
     
@@ -91,13 +91,13 @@ console.log(epochIds)
     
   };
   const setPlatformFeeAccount = async () => {
-  /*   await contract2.methods
+    /* await contract2.methods
     .setPlatformFeeAccount(fee)
     .send({
       from: account[0],
       value: amount * 10 ** 18,
       gas:3000000,
-    })  */
+    }) */ 
    
 
     
